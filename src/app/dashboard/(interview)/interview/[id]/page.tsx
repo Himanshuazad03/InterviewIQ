9786d332e3a54417ca26fe348b31961f65ef34dd
Himanshuazad03 <himanshuazad05@gmail.com>
2026-03-21 00:04:14 +0530

    updated

--- src/app/dashboard/(interview)/interview/[id]/page.tsx
+++ src/app/dashboard/(interview)/interview/[id]/page.tsx
@@ -1,13 +1,20 @@
 import { prisma } from "@/lib/prisma";
 import VoiceInterview from "@/components/interviewDash/VoiceInterview";
 import { getInterview } from "@/actions/Interview";
-
+import { checkRetakeRateLimit } from "@/actions/Interview";
+import { useRouter } from "next/navigation";
 export default async function InterviewPage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  const router = useRouter();
+  const rateLimit = await checkRetakeRateLimit();
+  if (!rateLimit.success) {
+    router.push("/dashboard/interviews");
+    return;
+  }
   const interview = await getInterview(id);
   if (!interview) {
     return <div>Interview not found.</div>;
